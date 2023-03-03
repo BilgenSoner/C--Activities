4fdf1f69e2610f4ac902d990858fe1362a44fda7
Soner BİLGEN <91529754+BilgenSoner@users.noreply.github.com>
2023-03-03 14:52:19 +0300

    .

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Database database = new SqlServer();
+        database.Added();
+        database.Delete();
+
+       Database database1= new Oracle();
+        database1.Added();
+        database1.Delete();
+
+        Console.ReadLine();
+    }
+}
+abstract class Database
+{
+    public void Added()
+    {
+        Console.WriteLine("Default Added");
+    }
+    public abstract void Delete();
+}
+
+class SqlServer : Database
+{
+    public override void Delete()
+    {
+        Console.WriteLine("Deleted by Sql");
+    }
+}
+class Oracle : Database
+{
+    public override void Delete()
+    {
+       Console.WriteLine("Deleted by Oracle");
+    }
+}
