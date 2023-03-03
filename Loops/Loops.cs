bc30f0b7fc92dcefca0bb08b8533a76c4df5188d
Soner BİLGEN <91529754+BilgenSoner@users.noreply.github.com>
2023-03-03 14:49:49 +0300

    .

---
+++ Loops/Loops.cs
@@ -0,0 +1,50 @@
+﻿internal class Loops
+{
+    private static void Main(string[] args)
+    {
+        if (IsPrimeNumber(8))
+        {
+            Console.WriteLine("this is Prime number");
+        }
+        else
+        {
+            Console.WriteLine("this not Prime number");
+        }
+        /*
+            for(int i=0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        */
+        /*
+            int s1 = 100;
+            while (s1>=0)
+            {
+                Console.WriteLine(s1--);
+            }
+        */
+        /*
+            int s1 = 10;
+            do { 
+            Console.WriteLine(s1);
+            s1--;
+            }while (s1 >= 11);
+        */
+
+        Console.ReadLine();
+    }
+    public static bool IsPrimeNumber(int s1)
+    {
+        bool result = true;
+        for (int i = 2; i < s1 - 1; i++)
+        {
+            if (s1 % i == 0)
+            {
+                return false;
+                i = s1;
+            }
+
+        }
+        return result;
+    }
+}
