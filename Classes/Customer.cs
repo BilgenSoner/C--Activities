df861ad59a316c63196db52bb8a54ef50a8efb48
Soner BİLGEN <91529754+BilgenSoner@users.noreply.github.com>
2023-03-03 14:54:38 +0300

    .

---
+++ Classes/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        //field
+      //  public string Microsoft;
+
+        //property
+        public int ID { get; set; }
+     //   private string _FirstName;
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String City { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using Classes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Classes.CustomerManager customerManager = new Classes.CustomerManager();
+        customerManager.Add();
+        customerManager.Update();
+
+        Classes.ProductManager productManager = new Classes.ProductManager();
+        productManager.Add();
+        productManager.Update();
+
+        Customer customer = new Customer();
+        customer.ID = 1;
+        customer.FirstName = "Soner";
+        customer.LastName = "BİLGEN";
+        customer.City = "Bilecik";
+
+        Customer customer2 = new Customer
+        {
+            ID = 2,
+            FirstName = "Furkan",
+            LastName = "Yüksel",
+            City = "Antalya",
+        };
+
+        Console.ReadLine();
+    }
+    
+    
+}
