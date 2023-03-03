73bb0be8499986deed9c3b66276a0bf76cdbf1aa
Soner BİLGEN <91529754+BilgenSoner@users.noreply.github.com>
2023-03-03 14:55:54 +0300

    .

---
+++ Inheritances/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Person[] persons = new Person[2]
+        {
+           new Customer(),new Student()
+        };
+    }
+}
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+class Customer : Person   // interface den farklı olarak "Customer : Person" bir kere yapılabilir.
+{
+    public string City { get; set; }
+}
+class Student : Person  // örnek Bir çocugun bir tane babası olabilir.
+{
+    public string Departman { get; set; }
+}
