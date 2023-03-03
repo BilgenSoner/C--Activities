e4ea4d5fdd825ef27d572332349cf92d37166e76
Soner BİLGEN <91529754+BilgenSoner@users.noreply.github.com>
2023-03-03 14:58:13 +0300

    .

---
+++ Interfaces/Program.cs
@@ -0,0 +1,76 @@
+﻿using Interfaces;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //   InterfacesIntro();
+        //   Demo2();
+
+        ICustomerDal[] customerDals = new ICustomerDal[2]
+        {
+            new SqlCustomerDal(),
+            new OracleCustomerDal()
+        };
+
+        foreach (var customerDal in customerDals)
+        {
+            customerDal.Add();
+        }
+        Console.ReadLine();
+    }
+
+    private static void Demo2()
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add(new SqlCustomerDal());
+        customerManager.Add(new OracleCustomerDal());
+    }
+
+    private static void InterfacesIntro()
+    {
+        PersonManager manager = new PersonManager();
+        manager.Add(new Customer { Id = 1, FirstName = "Soner", LastName = "Bilgen", Address = "Bursa" });
+
+        Student student = new Student();
+        manager.Add(new Student { Id = 2, FirstName = "Başak", LastName = "Aslan", Departmant = "Computer Science" });
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+
+    }
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SqlServer sqlServer = new SqlServer();
+        sqlServer.Add();
+
+        mySql mySql = new mySql();
+        mySql.Add();
+
+        Console.ReadLine();
+
+    }
+    
+}
+class Database
+{
+    public virtual void Add()
+    {
+        Console.WriteLine("Added by default");
+    }
+    public virtual void Delete()
+    {
+        Console.WriteLine("Deleted by default");
+    }
+}
+class SqlServer : Database
+{
+    public override void Add()
+    {
+        Console.WriteLine("SqlServer method working in add()");
+        //base.Add();
+    }
+}
+class mySql : Database
+{
+
+}
